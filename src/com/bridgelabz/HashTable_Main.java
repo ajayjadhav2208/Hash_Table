de7cbd8a18e5d93131f8105fccc9e1843483a4ea
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-06 12:46:44 +0530

    First UC Case

---
+++ src/com/bridgelabz/HashTable_Main.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class HashTable_Main {
+
+    public static void main(String[] args) {
+        // Welcome to Hashtable program
+
+        HashTable hashTable = new HashTable(10);
+        hashTable.put(10, "To");
+        hashTable.put(20, "be");
+        hashTable.put(30, "or");
+        hashTable.put(40, "not");
+        hashTable.put(50, "to");
+        hashTable.put(60, "be");
+
+        System.out.println(hashTable.freqChecker("be"));
+    }
+}
