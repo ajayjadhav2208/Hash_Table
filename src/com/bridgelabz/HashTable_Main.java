d8d74639cfa04e62e9bdb7ec9b507e17f604e200
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-13 17:52:10 +0530

    Second UC Case

--- src/com/bridgelabz/HashTable_Main.java
+++ src/com/bridgelabz/HashTable_Main.java
@@ -1,18 +1,28 @@
 package com.bridgelabz;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HashTable_Main {
 
     public static void main(String[] args) {
-        // Welcome to Hashtable program
 
-        HashTable hashTable = new HashTable(10);
-        hashTable.put(10, "To");
-        hashTable.put(20, "be");
-        hashTable.put(30, "or");
-        hashTable.put(40, "not");
-        hashTable.put(50, "to");
-        hashTable.put(60, "be");
+        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situation";
 
-        System.out.println(hashTable.freqChecker("be"));
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            // containsKey(key) will return a boolean value
+            // i.e. true if it contains the key and false if
+            // it doesn't.
+            if (hashMap.containsKey(word))
+                hashMap.put(word, hashMap.get(word) + 1);
+
+            else
+                hashMap.put(word, 1);
+        }
+
+        System.out.println(hashMap);
     }
 }
